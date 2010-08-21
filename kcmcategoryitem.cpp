99a581dd186c7059dabf44ef3a43c839313ebe9a
David Stephen Hubner <hubnerd@ntlworld.com>
2010-08-21 06:42:43 +0000

    SVN_SILENT: Make categories translatable, fixes 245216
    svn path=/trunk/KDE/kdebase/workspace/; revision=1166146

---
+++ kcmcategoryitem.cpp
@@ -0,0 +1,96 @@
+
+/*
+ *  kcmcategoryitem.cpp
+ *
+ *  Copyright (C) 2010 David Hubner <hubnerd@ntlworld.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+#include "kcmcategoryitem.h"
+
+KcmCategoryItem::KcmCategoryItem(const KService::Ptr module, KcmTreeItem *parent) : 
+  KcmTreeItem(module, parent), 
+  m_category(NULL) 
+{
+}
+
+KcmCategoryItem::KcmCategoryItem(QString categoryName) : 
+  KcmTreeItem(), 
+  m_category(categoryName)
+{
+}
+
+QString KcmCategoryItem::data()
+{
+  if(m_category == NULL)
+  {
+    return m_moduleInfo->moduleName();
+  }
+  else 
+  {
+    return category();
+  }
+}
+
+KcmTreeItem::itemType KcmCategoryItem::type() const
+{
+    return CATEGORY;
+}
+
+QString KcmCategoryItem::category()
+{
+  if(m_category == NULL)
+  {
+    return m_module->property("X-KDE-KInfoCenter-Category").toString().trimmed();  
+  } 
+  else 
+  {
+    return m_category;
+  }
+}
+
+const KCModuleInfo KcmCategoryItem::kcm() const 
+{
+  return KCModuleInfo();
+}
+
+int KcmCategoryItem::weight() 
+{
+  return (category().count() + 1000);
+}
+
+KIcon KcmCategoryItem::icon() const
+{
+  if(m_category == NULL)
+  {
+    return KIcon(m_moduleInfo->icon());
+  }
+  else 
+  {
+    return KIcon("");
+  }
+}
+
+QString KcmCategoryItem::whatsThis() const
+{
+  return QString();
+}
+
+QStringList KcmCategoryItem::keywords()
+{
+  return QStringList();
+}
---
+++ kcmcategoryitem.h
@@ -0,0 +1,98 @@
+
+/*
+ * kcmcategoryitem.h
+ *
+ *  Copyright (C) 2010 David Hubner <hubnerd@ntlworld.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+#ifndef KCMCATEGORYITEM
+#define KCMCATEGORYITEM
+
+#include "kcmtreeitem.h"
+
+//KDE
+#include <KCModuleInfo>
+#include <KIcon>
+
+class KcmCategoryItem : public KcmTreeItem
+{
+  public:
+   
+    /**
+     * Category Tree Item for InfoKcmModel. Holds information about a Category
+     *
+     * @param module pointer to KCM module
+     * @param parent objects parent
+     */
+    explicit KcmCategoryItem(const KService::Ptr module, KcmTreeItem *parent=0);
+    
+    /**
+     * Category Tree Item for InfoKcmModel. Holds information about a Category  
+     *
+     * @note Used for root folder objects
+     */
+    explicit KcmCategoryItem(QString category);
+    
+    /**
+     * Get data of tree item
+     */
+    QString data();
+    
+    /**
+     * Get category of tree item.
+     * Set in X-KDE-KInfoCenter-Category
+     */
+    QString category();
+    
+    /**
+     * Gets the item type.
+     */
+    itemType type() const;
+    
+    /**
+     * Get tree item KCMs Data
+     */
+    const KCModuleInfo kcm() const;
+    
+    /**
+     * Get tree items KCM's weight
+     */
+    int weight();
+    
+    /**
+     * Get icon tied to KCM
+     */
+    KIcon icon() const;
+    
+    /**
+     * Get whatsThis information from KCM
+     */
+    QString whatsThis() const;
+    
+    /**
+     * Get KCM tree item keywords
+     */
+    QStringList keywords();
+    
+  private:
+    
+    const QString m_category;
+};
+
+#endif //KCMCATEGORYITEM
+
