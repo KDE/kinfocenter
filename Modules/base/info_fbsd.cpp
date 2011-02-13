8ac336e4ed4318c61b4e42011482ef251fe02430
Raphael Kubo da Costa <kubito@gmail.com>
2011-02-13 01:20:46 -0200

    kinfocenter (FreeBSD): Remove checks for old PCI-related programs.
    /usr/X11R6 does not even exist anymore, so it does not make sense to
    look for programs there. We should only check if pciconf exists.
    
    Original patch by Max Brazhnikov from the KDE on FreeBSD team.
    
    CCMAIL: makc@freebsd.org

--- Modules/base/info_fbsd.cpp
+++ Modules/base/info_fbsd.cpp
@@ -216,23 +216,11 @@ bool GetInfo_PCI(QTreeWidget* tree) {
 	pcicontrol = new QFile("/usr/sbin/pciconf");
 
 	if (!pcicontrol->exists()) {
+		QStringList list;
+		list << i18n("Could not find any programs with which to query your system's PCI information");
+		new QTreeWidgetItem(tree, list);
 		delete pcicontrol;
-		pcicontrol = new QFile("/usr/X11R6/bin/scanpci");
-		if (!pcicontrol->exists()) {
-			delete pcicontrol;
-			pcicontrol = new QFile("/usr/X11R6/bin/pcitweak");
-			if (!pcicontrol->exists()) {
-				QStringList list;
-				list << i18n("Could not find any programs with which to query your system's PCI information");
-				new QTreeWidgetItem(tree, list);
-				delete pcicontrol;
-				return true;
-			} else {
-				cmd = "/usr/X11R6/bin/pcitweak -l 2>&1";
-			}
-		} else {
-			cmd = "/usr/X11R6/bin/scanpci";
-		}
+		return true;
 	} else {
 		cmd = "/usr/sbin/pciconf -l -v 2>&1";
 	}
