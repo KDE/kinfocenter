22befd06d9468d6300c65de47bba30db559fca75
Aurélien Gâteau <agateau@kde.org>
2013-03-27 18:41:59 +0100

    Rework layout

--- src/Module.cpp
+++ src/Module.cpp
@@ -114,7 +114,7 @@ void Module::load()
         ui->kernelLabel->setText(utsName.release);
 
     int bits = QT_POINTER_SIZE == 8 ? 64 : 32;
-    ui->bitsLabel->setText(i18n("%1-bit System", QString::number(bits)));
+    ui->bitsLabel->setText(i18n("%1-bit", QString::number(bits)));
 
     const QList<Solid::Device> list = Solid::Device::listFromType(Solid::DeviceInterface::Processor);
     // Only care about one processor/core...
