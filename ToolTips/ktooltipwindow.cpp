fab69c9d83aed6cb63986a42868c8635cba6577c
Hugo Pereira Da Costa <hugo.pereira@free.fr>
2014-08-26 16:15:05 +0200

    Use style primitive to render custom tooltip rather than custom code REVIEW: 119940

--- ToolTips/ktooltipwindow.cpp
+++ ToolTips/ktooltipwindow.cpp
@@ -24,7 +24,9 @@
 #include <kcolorscheme.h>
 #include <kwindowsystem.h>
 
-#include <QPainter>
+#include <QPaintEvent>
+#include <QStylePainter>
+#include <QStyleOptionFrame>
 #include <QVBoxLayout>
 
 KToolTipWindow::KToolTipWindow(QWidget* content) :
@@ -43,47 +45,10 @@ KToolTipWindow::~KToolTipWindow()
 
 void KToolTipWindow::paintEvent(QPaintEvent* event)
 {
-    Q_UNUSED(event);
-
-    QPainter painter(this);
-
-    QColor toColor = palette().brush(QPalette::ToolTipBase).color();
-    QColor fromColor = KColorScheme::shade(toColor, KColorScheme::LightShade, 0.2);
-
-    const bool haveAlphaChannel = KWindowSystem::compositingActive();
-    if (haveAlphaChannel) {
-        painter.setRenderHint(QPainter::Antialiasing);
-        painter.translate(0.5, 0.5);
-        toColor.setAlpha(220);
-        fromColor.setAlpha(220);
-    }
-
-    QLinearGradient gradient(QPointF(0.0, 0.0), QPointF(0.0, height()));
-    gradient.setColorAt(0.0, fromColor);
-    gradient.setColorAt(1.0, toColor);
-    painter.setPen(Qt::NoPen);
-    painter.setBrush(gradient);
-
-    const QRect rect(0, 0, width(), height());
-    if (haveAlphaChannel) {
-        const qreal radius = 5.0;
-
-        QPainterPath path;
-        path.moveTo(rect.left(), rect.top() + radius);
-        arc(path, rect.left()  + radius, rect.top()    + radius, radius, 180, -90);
-        arc(path, rect.right() - radius, rect.top()    + radius, radius,  90, -90);
-        arc(path, rect.right() - radius, rect.bottom() - radius, radius,   0, -90);
-        arc(path, rect.left()  + radius, rect.bottom() - radius, radius, 270, -90);
-        path.closeSubpath();
-
-        painter.drawPath(path);
-    } else {
-        painter.drawRect(rect);
-    }
+    QStylePainter painter(this);
+    painter.setClipRegion(event->region());
+    QStyleOptionFrame option;
+    option.init(this);
+    painter.drawPrimitive(QStyle::PE_PanelTipLabel, option);
+    painter.end();
 }
-
-void KToolTipWindow::arc(QPainterPath& path, qreal cx, qreal cy, qreal radius, qreal angle, qreal sweeplength)
-{
-    path.arcTo(cx-radius, cy-radius, radius * 2, radius * 2, angle, sweeplength);
-}
-
