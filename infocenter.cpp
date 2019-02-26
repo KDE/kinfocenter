KDE/kinfocenter

commit 5e27d9476e71d6e51c433e4edde3c403f6242aa8

Allow to select module with keyboard
Only the "clicked" signal was being tracked by the side panel. As a result
if the user wanted to select an element with the keyboard it would be ignored.

"activated" signal cannot be used exclusively as in that case if the user has
configured the system to require double click to open files "activated" will
not be called when a single click is done.

For that reason, both "clicked" and "activated" are tracked and
KInfoCenter::setKcm has been modified to not reload the module if it's the
same module that is already loaded.

infocenter.cpp
@@ -202,10 +202,12 @@ void KInfoCenter::itemClickedSlot(const KcmTreeItem *item)
 
 void KInfoCenter::setKcm(const KcmTreeItem *kcmItem)
 {
-    m_contain->setKcm(kcmItem->kcm());
+    if (m_contain->fileName() != kcmItem->kcm().fileName()) {
+        m_contain->setKcm(kcmItem->kcm());
 
-    setButtons(m_contain->buttons());
-    m_aboutKcm->setEnabled(true);
+        setButtons(m_contain->buttons());
+        m_aboutKcm->setEnabled(true);
+    }
 }
 
 void KInfoCenter::setButtons(const KCModule::Buttons buttons)

sidepanel.cpp
@@ -46,11 +46,13 @@ SidePanel::SidePanel(QWidget *parent) : QTreeView(parent)
 
     setMouseTracking(true);
     setModel(m_proxyModel);
+    connect(this,SIGNAL(activated(QModelIndex)),this,SLOT(clickedSlot(QModelIndex)));
     connect(this,SIGNAL(clicked(QModelIndex)),this,SLOT(clickedSlot(QModelIndex)));
 }
 
 SidePanel::~SidePanel()
 {
+    disconnect(this,SIGNAL(activated(QModelIndex)),this,SLOT(clickedSlot(QModelIndex)));
     disconnect(this,SIGNAL(clicked(QModelIndex)),this,SLOT(clickedSlot(QModelIndex)));
 
     delete m_proxyModel;

commit 585b92af8f0e415ffb8c2cb6a4712a8fe01fbbc4

unbreak help button activation by not double-resetting the view
Summary:
when sidepanel clickedSlot gets called it issues clicked() and that causes
a reset of the infocenter state (among other things disabling the help
button). previously this reset would happen all the time.
if you clicked the same kcm in the sidepanel twice the first click would
reset and then activate the help button, the second click would reset and
then not enable the help button because the kcm is already loaded (see
setKcm).

this broke in 5e27d9476e71d6e51c433e4edde3c403f6242aa8 which caused every
click event to get signaled twice for every single entry (thus always
resulting in a double reset) AND the commit also introduced
the reload-guard in setKcm which ultimately resulted in the entirely broken
behavior of resetting the UI twice but only loading kcm-dependent states
once

to resolve this problem the actual selected Item* is now held in the
InfoCenter and when a click is received it will only result in a UI reset
iff the clicked item isn't already the active item. the check in setKcm
is replaced by this.
additionally the sidepanel now only connects to activated() signals as
activation also includes activation-by-click rendering the second
connection a duplicate.

BUG: 392597
FIXED-IN: 5.15.2
CHANGELOG: The "Module Help" button gets enabled when help is available

Test Plan:
- select memory module (has documention)
- module help button enabled
- select about module (has no documentation)
- help disabled
- keyboard navigate to memory module
- help enabled
- click to double-activate the memory module
- help stays enabled

Reviewers: #plasma, mart

Reviewed By: #plasma, mart

Subscribers: apol, plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D19187

infocenter.cpp
@@ -200,20 +200,21 @@ void KInfoCenter::clearSearchLine()
 
 void KInfoCenter::itemClickedSlot(const KcmTreeItem *item)
 {
-	resetCondition();
-	if(item->type() != KcmTreeItem::CATEGORY) {
-        setKcm(item);
+    if (item != m_kcmItem) {
+        resetCondition();
+        if(item->type() != KcmTreeItem::CATEGORY) {
+            setKcm(item);
+        }
     }
+    m_kcmItem = item;
 }
 
 void KInfoCenter::setKcm(const KcmTreeItem *kcmItem)
 {
-    if (m_contain->fileName() != kcmItem->kcm().fileName()) {
-        m_contain->setKcm(kcmItem->kcm());
+    m_contain->setKcm(kcmItem->kcm());
 
-        setButtons(m_contain->buttons());
-        m_aboutKcm->setEnabled(true);
-    }
+    setButtons(m_contain->buttons());
+    m_aboutKcm->setEnabled(true);
 }
 
 void KInfoCenter::setButtons(const KCModule::Buttons buttons)

infocenter.h
@@ -141,6 +141,7 @@ class KInfoCenter : public KXmlGuiWindow
 
         void clearSearchLine();
 
+        const KcmTreeItem *m_kcmItem = nullptr;
         KcmContainer *m_contain = nullptr;
         QSplitter *m_splitter = nullptr;
 

sidepanel.cpp
@@ -48,13 +48,11 @@ SidePanel::SidePanel(QWidget *parent)
     setMouseTracking(true);
     setModel(m_proxyModel);
     connect(this,&QAbstractItemView::activated,this,&SidePanel::clickedSlot);
-    connect(this,SIGNAL(clicked(QModelIndex)),this,SLOT(clickedSlot(QModelIndex)));
 }
 
 SidePanel::~SidePanel()
 {
     disconnect(this,&QAbstractItemView::activated,this,&SidePanel::clickedSlot);
-    disconnect(this,SIGNAL(clicked(QModelIndex)),this,SLOT(clickedSlot(QModelIndex)));
 
     delete m_proxyModel;
     delete m_model;