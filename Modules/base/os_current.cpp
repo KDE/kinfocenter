43cd7a26f31afa3b1cee1c2189ac0597d0405fb3
Harald Sitter <sitter@kde.org>
2020-01-30 13:13:00 +0100

    remove all platform conditionals not pertaining to linux or *bsd
    Summary:
    we do not support plasma on anything else, nor do we CI on other platforms,
    so all these conditionals were unnecessary complexity and code nobody needs
    
    Test Plan: still builds on linux
    
    Reviewers: broulik, nicolasfella
    
    Reviewed By: nicolasfella
    
    Subscribers: plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D27030

--- Modules/base/os_current.cpp
+++ Modules/base/os_current.cpp
@@ -24,23 +24,13 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 #include "os_base.h"
 
 #ifdef __linux__
-	#include "info_linux.cpp"
-#elif defined(sgi) && sgi
-	#include "info_sgi.cpp"
+    #include "info_linux.cpp"
 #elif defined(__FreeBSD__) || defined(__DragonFly__)
-	#include "info_fbsd.cpp"
-#elif defined(__hpux)
-	#include "info_hpux.cpp"
+    #include "info_fbsd.cpp"
 #elif defined(__NetBSD__)
 	#include "info_netbsd.cpp"
 #elif defined(__OpenBSD__)
-	#include "info_openbsd.cpp"
-#elif defined(__svr4__) && defined(sun)
-	#include "info_solaris.cpp"
-#elif defined(_AIX)
-	#include "info_aix.cpp"
-#elif defined(__APPLE__)
-	#include "info_osx.cpp"
+    #include "info_openbsd.cpp"
 #else
 	#include "info_generic.cpp"	/* Default for unsupported systems.... */
 #endif
