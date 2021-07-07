0e5e4567141b814f67ef4e6971121ba9b922b265
Harald Sitter <sitter@kde.org>
2021-07-07 13:52:02 +0200

    unbreak about CPU value when solid is missing a product string
    instead of `Processors: 4 x` one now gets `Processors: 4`, or
    `Processors: 4 x Unknown Type, 12 x AMD Ryzen 5 3600X 6-Core Processor`
    when some of the cores have a product and the others do not.
    
    The latter is an incredible edge case but I suppose we best handle it
    properly. It did require reshuffling of the entry though. We now list
    the cores in the ctor to generate the label but only generate the values
    at call time so as to localize this potential placeholder accordingly.
    
    Meanwhile the former case ultimately lumps all processors together even
    when they are actually different from a physical POV (such as the
    heterogenous CPU set up of "ARM big.LITTLE"), but given the high level
    overview this KCM provides there is no point in expansively trying to
    figure out what the actual topology is (we do indeed not concern
    ourselves with topology at all in this entry).
    
    BUG: 439464
    FIXED-IN: 5.22.4

--- Modules/about-distro/src/CPUEntry.h
+++ Modules/about-distro/src/CPUEntry.h
@@ -1,10 +1,12 @@
 /*
-    SPDX-FileCopyrightText: 2012-2020 Harald Sitter <sitter@kde.org>
+    SPDX-FileCopyrightText: 2012-2021 Harald Sitter <sitter@kde.org>
     SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 */
 
 #ifndef CPUENTRY_H
 #define CPUENTRY_H
+
+#include <solid/device.h>
 
 #include "Entry.h"
 
@@ -12,6 +14,10 @@ class CPUEntry : public Entry
 {
 public:
     CPUEntry();
+    QString localizedValue(Language language = Language::System) const override;
+
+private:
+    const QList<Solid::Device> m_cpus = Solid::Device::listFromType(Solid::DeviceInterface::Processor);
 };
 
 #endif // CPUENTRY_H
