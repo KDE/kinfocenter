4a4e6320309bf1b107d88667b005a01f6a54d79a
Harald Sitter <sitter@kde.org>
2021-08-11 12:55:13 +0200

    refactor runtime program finding
    instead of having dupe logic let's have a helper module that macros the
    program finding. this also paves the way for more vulkaninfo-style KCMs
    that call a binary at runtime and thus have a runtime requirement

--- cmake/Findvulkaninfo.cmake
+++ cmake/Findvulkaninfo.cmake
@@ -6,11 +6,5 @@
 # SPDX-License-Identifier: BSD-2-Clause
 # SPDX-FileCopyrightText: 2021 Harald Sitter <sitter@kde.org>
 
-find_program(vulkaninfo_PATH vulkaninfo)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(vulkaninfo
-    FOUND_VAR vulkaninfo_FOUND
-    REQUIRED_VARS vulkaninfo_PATH
-)
-mark_as_advanced(vulkaninfo_PATH)
+include(ProgramFinder)
+program_finder(vulkaninfo)
