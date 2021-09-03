5ab22ea18fa05482e233a2b375eb83ad4d8a932c
Harald Sitter <sitter@kde.org>
2021-09-03 11:34:37 +0200

    construct GL renderer string from unicode
    as it turns out we wrongly assumed the value would be latin1 when indeed
    the output may be unicode (e.g. include registered trademark symbols)
    
    BUG: 441894
    FIXED-IN: 5.23

--- Modules/about-distro/src/GPUEntry.cpp
+++ Modules/about-distro/src/GPUEntry.cpp
@@ -1,7 +1,6 @@
 /*
     SPDX-FileCopyrightText: 2016 Rohan Garg <rohan@kde.org>
-
-    SPDX-FileCopyrightText: 2020 Harald Sitter <sitter@kde.org>
+    SPDX-FileCopyrightText: 2020-2021 Harald Sitter <sitter@kde.org>
     SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 */
 
@@ -28,7 +27,7 @@ GPUEntry::GPUEntry()
     }
 
     if (context.makeCurrent(&surface)) {
-        m_value = QString::fromLatin1(reinterpret_cast<const char *>(context.functions()->glGetString(GL_RENDERER)));
+        m_value = QString::fromUtf8(reinterpret_cast<const char *>(context.functions()->glGetString(GL_RENDERER)));
         m_value = FancyString::fromUgly(m_value);
         // It seems the renderer value may have excess information in parentheses ->
         // strip that. Elide would probably be nicer, a bit meh with QWidgets though.
