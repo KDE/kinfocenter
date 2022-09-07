475779980a50231e0f62981aeaabe5ca76e0f6fe
Nate Graham <nate@kde.org>
2022-09-07 07:37:50 -0600

    Add KWin Support Information KCM
    This should help bug reporters gather relevant KWin information more
    easily.
    
    co-authored-by: Harald Sitter <sitter@kde.org>

---
+++ Modules/kwinsupportinfo/main.cpp
@@ -0,0 +1,38 @@
+/*
+    SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+    SPDX-FileCopyrightText: 2021 Harald Sitter <sitter@kde.org>
+    SPDX-FileCopyrightText: 2022 Nate Graham <nate@kde.org>
+*/
+
+#include <KAboutData>
+#include <KLocalizedString>
+#include <KPluginFactory>
+#include <KQuickAddons/ConfigModule>
+
+#include <CommandOutputContext.h>
+
+class KCMKWinSupportInfo : public KQuickAddons::ConfigModule
+{
+    Q_OBJECT
+public:
+    explicit KCMKWinSupportInfo(QObject *parent, const KPluginMetaData &data, const QVariantList &args)
+        : ConfigModule(parent, data, args)
+    {
+        auto outputContext = new CommandOutputContext(QStringLiteral("qdbus"),
+                                                      {QStringLiteral("org.kde.KWin"), QStringLiteral("/KWin"), QStringLiteral("supportInformation")},
+                                                      parent);
+        qmlRegisterSingletonInstance("org.kde.kinfocenter.kwinsupportinfo.private", 1, 0, "InfoOutputContext", outputContext);
+
+        auto *about = new KAboutData(QStringLiteral("kcm_kwinsupportinfo"),
+                                     i18nc("@label kcm name", "Window Manager"),
+                                     QStringLiteral("1.0"),
+                                     QString(),
+                                     KAboutLicense::GPL);
+        about->addAuthor(i18n("Nate Graham"), QString(), QStringLiteral("nate@kde.org"));
+        setAboutData(about);
+    }
+};
+
+K_PLUGIN_CLASS_WITH_JSON(KCMKWinSupportInfo, "kcm_kwinsupportinfo.json")
+
+#include "main.moc"
---
+++ Modules/kwinsupportinfo/package/contents/ui/main.qml
@@ -0,0 +1,18 @@
+/*
+    SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+    SPDX-FileCopyrightText: 2021 Harald Sitter <sitter@kde.org>
+    SPDX-FileCopyrightText: 2022 Nate Graham <nate@kde.org>
+*/
+
+import QtQuick 2.5
+
+import org.kde.kcm 1.4 as KCM
+
+import org.kde.kinfocenter.private 1.0 as KInfoCenter
+import org.kde.kinfocenter.kwinsupportinfo.private 1.0
+
+KInfoCenter.CommandOutputKCM {
+    KCM.ConfigModule.quickHelp: i18nc("@info", "KWin Support Information")
+
+    output: InfoOutputContext
+}
