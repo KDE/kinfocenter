2b27c817d03f31f3456fbea44081d9a86588d5f8
Albert Astals Cid <aacid@kde.org>
2023-10-04 22:23:29 +0200

    Remove unneeded Messages.sh
    They give out errors on executing and there's no i18n in those folders

--- Modules/glx/Messages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# SPDX-License-Identifier: BSD-3-Clause
-# SPDX-FileCopyrightText: 2020-2021 Harald Sitter <sitter@kde.org>
-
-$XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/kcm_glx.pot
-# Extract JavaScripty files as what they are, otherwise for example template literals won't work correctly (by default we extract as C++).
-# https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-$XGETTEXT --join-existing --language=JavaScript `find . -name \*.qml -o -name \*.js` -o $podir/kcm_glx.pot
