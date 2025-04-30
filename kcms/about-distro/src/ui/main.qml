KDE/kinfocenter

commit 89ff29778f8e4c9c3ecb712bb47f92ad9be5b735

about-distro: move kinfocenter button to header to improve consistency

kcms/about-distro/src/ui/main.qml
@@ -151,25 +151,18 @@ KCM.SimpleKCM {
                 delegate: entryComponent
             }
         }
-
-        Item {
-            visible: !kcm.isThisKInfoCenter && kicRunner.canRun
-            implicitHeight: Kirigami.Units.largeSpacing
-        }
-
-        QQC2.Button {
-            visible: !kcm.isThisKInfoCenter && kicRunner.canRun
-
-            Layout.alignment: Qt.AlignHCenter
-
-            text: i18nc("@action:button launches kinfocenter from systemsettings", "Launch %1", kicRunner.genericName)
-            icon.name: kicRunner.iconName
-            onClicked: kicRunner.run()
-        }
     }
 
 
     actions: [
+        Kirigami.Action {
+            visible: !kcm.isThisKInfoCenter && kicRunner.canRun
+
+            icon.name: kicRunner.iconName
+            text: i18nc("@action:button launches kinfocenter from systemsettings", "More System Information")
+            tooltip: i18nc("@info:tooltip", "Launch %1", kicRunner.genericName)
+            onTriggered: kicRunner.run()
+        },
 
         Kirigami.Action {
             visible: kcm.isEnglish

commit 556e36c6ec5ae49b6756450ad73b730765ea404e

kcms/about-distro: Reword "More System Information" button per HIG
Change button label to say what it's actually going to do, which is launching kinfocenter, in line with HIG. Also to make the tooltip text not redundant, change it to be a bit more descriptive.

This was apparently already done before in 20f62bdc56be09f69f5d580af3d6e3c520b3c062 and regressed in 89ff29778f8e4c9c3ecb712bb47f92ad9be5b735

kcms/about-distro/src/ui/main.qml
@@ -215,8 +215,8 @@ KCMUtils.SimpleKCM {
             visible: !kcm.isThisKInfoCenter && kicRunner.canRun
 
             icon.name: kicRunner.iconName
-            text: i18nc("@action:button launches kinfocenter from systemsettings", "More System Information")
-            tooltip: i18nc("@info:tooltip", "Launch %1", kicRunner.genericName)
+            text: i18nc("@action:button", "Launch %1…", kicRunner.genericName)
+            tooltip: i18nc("@info:tooltip launches kinfocenter from systemsettings", "See more detailed system information")
             onTriggered: source => kicRunner.run()
         },
 