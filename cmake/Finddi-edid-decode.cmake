fc076d02ab91e0a361f26258288f36325b64dc6b
Antonio Rojas <arojas@archlinux.org>
2025-05-17 18:30:48 +0200

    Add missing cmake find module for di-edid-decode
    Without this, cmake always reports it as not found so the check is useless

---
+++ cmake/Finddi-edid-decode.cmake
@@ -0,0 +1,10 @@
+# This module defines the following variables:
+#
+#  di-edid-decode_FOUND - true if found
+#  di-edid-decode_PATH - path to the bin (only when found)
+#
+# SPDX-License-Identifier: BSD-2-Clause
+# SPDX-FileCopyrightText: 2024 Harald Sitter <sitter@kde.org>
+
+include(ProgramFinder)
+program_finder(di-edid-decode)
